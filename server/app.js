0880c62b08ec3df0ffeb2a5b6b4993ccb37f70df
WadeeKT <thomassanna1e@gmail.com>
2023-06-12 18:29:07 +0200

    Initialisation server and client

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const sequelize = require('./db/sequelize');
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -14,5 +14,5 @@ sequelize.initdb();
 
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 5000');
 });
