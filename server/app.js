cbc3aaf47ecb15f0fc9d6db647d37b11b3827d29
WadeeKT <thomassanna1e@gmail.com>
2023-06-16 08:31:37 +0200

    searching to resolve req.session issues.

--- server/app.js
+++ server/app.js
@@ -6,7 +6,8 @@ const session = require("express-session"); // npm install express-session
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-const router = express.Router();
+
+app.set('trust proxy', 1)
 
 app
   .use(bodyParser.urlencoded({ extended: false }))
@@ -21,36 +22,43 @@ app
   .use(session({
   secret: "blablabalbauheuhuhuaizheiuhuizheiu",
   resave: true,
-  saveUninitialized: false,
-  rolling: true, // set rolling to false to prevent session from being reset on page refresh
+  saveUninitialized: true,
+  rolling: false,
   cookie: { 
-    secure: false,
     maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
   }
   }))
-  .use(function (req, res, next) {
-    res.locals.user = req.session.user;
-    next();
-  })
-
 
 sequelize.initdb();
 
 require("./routes/register")(app);
-require("./routes/logout")(app);
-require("./routes/getUser")(app);
 
 
 
 app.get("/", (req, res) => {
-  console.log('session', req.session.user);
-  if (req.session.user) {
-    return res.json({ username: req.session });
+  console.log('onGetSession', req.session);
+  if (req.session.userId) {
+    return res.json({
+      'UserId': req.session,
+      'Views': req.session.views,
+    });
   } else {
-    return res.json({ username: null });
+    return res.json({"id": "No id found"});
   }
 })
 
+app.get('/test', (req, res, next) => {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+})
 
 
 app.listen(PORT, () => {
